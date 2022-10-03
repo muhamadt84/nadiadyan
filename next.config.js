00202c5948c2d4d57d9cc0d061030314aabd1f00
taufik <taufik@codelabs.co.id>
2022-10-03 15:39:09 +0700

    update headers cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "http://178.128.90.130" },
+        ],
+      },
+    ]
+  },
   env: {
     urlAPI: 'http://178.128.90.130/pf/pf_api/',
   },
   reactStrictMode: true,
   swcMinify: true,
+  
 }
 
 module.exports = nextConfig
